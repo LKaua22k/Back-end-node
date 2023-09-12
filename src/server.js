a61632d6b115e65e6e9c786bc654635acd87bce0
Lkaua22k <lkauan462@gmail.com>
2023-09-11 23:44:58 -0300

    Criando Rotas :tada:

--- src/server.js
+++ src/server.js
@@ -2,7 +2,19 @@ const express = require('express')
 
 const app = express()
 
-const port = 3333;
+app.get("/", (req,res) => {
+    res.send("hello world")
+})
+
+app.get("/params/:id/:user", (req,res) => {
+
+    const {id , user} = req.params
+
+    res.send(`Isso é um route params, seu id é ${id} e seu usuario é ${user}`)
+})
 
 
-app.listen(port , ()=>{console.log('Serve rodando')})
+
+const PORT = 3333;
+
+app.listen(PORT , ()=>{console.log('Serve rodando')})

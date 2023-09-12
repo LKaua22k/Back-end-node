b66f499ba0cab234f26acaccaaa795b21eaf513b
Lkaua22k <lkauan462@gmail.com>
2023-09-12 20:04:57 -0300

    :tada:

--- src/routes/newusers.routes.js
+++ src/routes/newusers.routes.js
@@ -9,12 +9,4 @@ const newusers = Router()
 
 newusers.post("/", userController.create)
 
-newusers.post("/", (req,res) => {
-    const {name , email} = req.body
-    
-
-    res.json({name,email});
-})
-
-
 module.exports = newusers;

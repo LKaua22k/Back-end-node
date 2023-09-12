ed1d708fd93741e53cc3648b4c0ee8e34e25d70c
Kauã Araujo de Lima <112039006+LKaua22k@users.noreply.github.com>
2023-09-12 16:31:37 -0300

    :tada:

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,8 @@
 const { Router } = require('express')
 
-const userRouter = require('./user.routes')
 const newusers = require('./newusers.routes')
 
 const routes = Router()
-
-routes.use("/users", userRouter)
 
 routes.use("/newusers", newusers)
 
--- src/routes/newusers.routes.js
+++ src/routes/newusers.routes.js
@@ -5,11 +5,8 @@ const newusers = Router()
 newusers.post("/", (req,res) => {
     const {name , email} = req.body
     
-    // const text = `Seu nome é ${name} e seu email é ${email}`
 
     res.json({name,email});
-
-    // res.send(text)
 })
 
 module.exports = newusers;

34f8b3b182d1e5db1dbf100ca0f14fc59017f8bf
Lkaua22k <lkauan462@gmail.com>
2023-09-12 00:01:41 -0300

    Utilizando querry params :tada:

--- src/server.js
+++ src/server.js
@@ -2,15 +2,25 @@ const express = require('express')
 
 const app = express()
 
+// Metod simples
 app.get("/", (req,res) => {
     res.send("hello world")
 })
 
+// Route params
 app.get("/params/:id/:user", (req,res) => {
 
     const {id , user} = req.params
 
     res.send(`Isso é um route params, seu id é ${id} e seu usuario é ${user}`)
+})
+
+// querry params
+app.get("/params", (req,res) => {
+
+    const {page , lemit} = req.query
+
+    res.send(`Isso é um Querry params, seu id é ${page} e seu usuario é ${lemit}`)
 })
 
 

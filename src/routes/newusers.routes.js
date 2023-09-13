198739ffbf062e72ec74e130be52a1208a30c2dc
Kauã Araujo de Lima <112039006+LKaua22k@users.noreply.github.com>
2023-09-13 11:58:42 -0300

    :tada:

--- src/routes/newusers.routes.js
+++ src/routes/newusers.routes.js
@@ -4,6 +4,17 @@ const UserController = require('../controllers/user.controller')
 
 const userController = new UserController()
 
+function myMiddlewarer(req,res,next){
+    console.log('Isso é um middleware')
+
+    // if(!req.body.isAdmin){
+    //     return res.json({"message": "Não autorizado"})
+    // }
+
+    // next()
+}
+
+
 const newusers = Router()
 
 

2ff30b0a523edbca372d1895aab78b0bca1a335f
Lkaua22k <lkauan462@gmail.com>
2023-09-14 22:40:17 -0300

    Craindo uma conexão com BD :tada:

--- src/server.js
+++ src/server.js
@@ -3,12 +3,15 @@ require('express-async-errors')
 const express = require('express')
 const routes = require('./routes')
 const AppError = require('./utils/App.Error')
+const database = require('./database/sqlite')
 
 
 const app = express()
 app.use(express.json())
 
 app.use(routes)
+
+database()
 
 app.use((error,req,res,next) => {
 

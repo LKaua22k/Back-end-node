b8d55859647b70a692001f06ced1117c1b347727
Kauã Araujo de Lima <112039006+LKaua22k@users.noreply.github.com>
2023-09-18 15:19:46 -0300

    Ajustando erros :tada:

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -15,7 +15,7 @@ class UserController {
 
         await database.run("INSERT INTO users (name,email,password) VALUES (?,?,?)",[name,email,password])
 
-        res.status(201).json()
+        return res.status(201).json()
     }
 }
 

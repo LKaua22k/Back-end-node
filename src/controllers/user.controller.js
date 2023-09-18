709933950afb849a1f12e1cff37049a10b597682
Kauã Araujo de Lima <112039006+LKaua22k@users.noreply.github.com>
2023-09-18 15:18:15 -0300

    :tada:

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,13 +4,15 @@ const AppError = require('../utils/App.Error')
 
 class UserController {
     async create(req,res){
+        const {name, email, password} = req.body
+
         const database = await sqlConnection()
         const checkUser = await database.get("SELECT * FROM users WHERE email = (?)",[email])
 
         if(checkUser){
             throw new AppError('Usuario ja existe')
         }
-        
+
         await database.run("INSERT INTO users (name,email,password) VALUES (?,?,?)",[name,email,password])
 
         res.status(201).json()

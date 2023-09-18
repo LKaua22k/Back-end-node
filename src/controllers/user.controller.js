83c8396e958ca8b0832f3e24c36ad1369b627125
Lkaua22k <lkauan462@gmail.com>
2023-09-18 14:27:51 -0300

    :tada:

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,14 +1,26 @@
+const sqlConnection = require('../database/sqlite')
 const AppError = require('../utils/App.Error')
 
 class UserController {
-    create(req,res){
+    async create(req,res){
         const {name , email} = req.body
 
-        if(!name){
-            throw new AppError("Nome é obrigatorio")
+        const database = await sqlConnection()
+
+        const checkUser = database.get("SELECT * FROM users WHERE email = (?)",[email])
+
+        if(!checkUser){
+            throw new AppError('Usuario ja existe')
         }
 
-        res.json({name,email});
+        res.status(201).json({})
+        
+        
+        // if(!name){
+        //     throw new AppError("Nome é obrigatorio")
+        // }
+
+        // res.json({name,email});
     }
 }
 

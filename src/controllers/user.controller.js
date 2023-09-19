271570d8f8ab3234f1247f0014e23ce82e900f16
Lkaua22k <lkauan462@gmail.com>
2023-09-18 23:31:09 -0300

    Criando rotas de atualização :tada:

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,16 +4,46 @@ const AppError = require('../utils/App.Error')
 
 class UserController {
     async create(req,res){
+
+        const {name,email,password} = req.body
+
         const database = await sqlConnection()
         const checkUser = await database.get("SELECT * FROM users WHERE email = (?)",[email])
+
+        const HashPassword = await hash(password, 8)
 
         if(checkUser){
             throw new AppError('Usuario ja existe')
         }
 
-        await database.run("INSERT INTO users (name,email,password) VALUES (?,?,?)",[name,email,password])
+        await database.run("INSERT INTO users (name,email,password) VALUES (?,?,?)",[name,email,HashPassword])
         
         res.status(201).json()
+    }
+
+    async update(req,res){
+        const { name , email} = req.body;   
+        const { id } = req.params;
+
+        const database = await sqlConnection()
+        const user = await database.get("SELECT * FROM users WHERE id = (?)",[id])
+
+        if(!user){
+            throw new AppError("Usuario não encontrado")
+        }
+
+        const UserUpdate = await database.get("SELECT * FROM users WHERE email = (?)",[email])
+
+        if(UserUpdate && UserUpdate.id !== user.id){
+            throw new AppError("Este email ja esta em uso")
+        }
+
+        user.name = name
+        user.email = email
+        
+        await database.run("UPDATE users SET name = ?, email = ?, update_att = ? WHERE id = ?",[user.name,user.email,new Date(),id])
+
+        return res.json()
     }
 }
 
--- src/routes/newusers.routes.js
+++ src/routes/newusers.routes.js
@@ -4,21 +4,10 @@ const UserController = require('../controllers/user.controller')
 
 const userController = new UserController()
 
-function myMiddlewarer(req,res,next){
-    console.log('Isso é um middleware')
-
-
-    if(!req.body.isAdmin){
-        return res.json({"message": "Não autorizado"})
-    }
-
-    next()
-}
-
-
 const newusers = Router()
 
 
-newusers.post("/", myMiddlewarer, userController.create)
+newusers.post("/", userController.create)
+newusers.put("/:id", userController.update)
 
 module.exports = newusers;

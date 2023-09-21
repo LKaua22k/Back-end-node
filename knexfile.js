947d5f608c0bf21bc69922b8744c918be04eb417
Lkaua22k <lkauan462@gmail.com>
2023-09-21 00:06:51 -0300

    Table notes criada :tada:

--- knexfile.js
+++ knexfile.js
@@ -9,6 +9,7 @@ module.exports = {
     },
     migrations: {
       directory: path.resolve(__dirname, "src" , "database", "knex", "migrations")
-    } 
+    },
+    useNullAsDefault: true 
   }, 
 };

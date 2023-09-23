7077d794bbdac6bc6a6144e1c5fd4f74baa6cb68
Kauã Araujo de Lima <112039006+LKaua22k@users.noreply.github.com>
2023-09-23 13:07:21 +0000

    Termiando o show :tada:

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -38,8 +38,14 @@ class NotesController {
         const { id } = req.params
 
         const note = await knex("notes").where({id}).first()
+        const tags = await knex("tags").where({note_id: id}).orderBy('name')
+        const links = await knex("links").where({note_id: id}).orderBy('created_at')
 
-        return res.json(note)
+        return res.json(
+            ...note,
+            tags,
+            links
+        )
     }
 }
 
